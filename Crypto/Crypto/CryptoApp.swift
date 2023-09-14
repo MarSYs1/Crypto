f09bc06fd0785f81bceb7c3b6df257b3c68c0182
Mauro de Oliveira André <mauroolivere@hotmail.com>
2023-09-14 08:13:10 +0100

    Primeiro Commit

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Mauro de Oliveira André on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
